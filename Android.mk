bc28764f9456e48be382de08497350552ac24fc5
mr-Kronnus <lp.meteora.striks@gmail.com>
2017-01-18 12:28:33 +0300

    Create Android.mk

---
+++ Android.mk
@@ -0,0 +1,5 @@
+LOCAL_PATH := $(my-dir)\\устанавливаем директорию
+include $(CLEAR_VARS)\\подключаем локальные переменные
+LOCAL_MODULE := hello_world \\Название модуля
+LOCAL_SRC_FILES := hello_world.c\\Файл с исходным кодом
+LOCAL_MODULE_TAGS := eng \\Только для пользовательских сборок.
